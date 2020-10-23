d9769b2a66f7431a4f4cf8c306bd269bccc26f02
vasiliyeskin <vasiliyeskin@yandex.ru>
2020-10-23 04:45:25 +0300

    outputs for the test

--- train_im2latex.py
+++ train_im2latex.py
@@ -99,6 +99,7 @@ def train(image, text, encoder, decoder, criterion, train_loader, teach_forcing_
 
             loss = 0.0
             teach_forcing = True if random.random() > teach_forcing_prob else False
+            print(f'teach_forcing: {0}', teach_forcing)
             if teach_forcing:
                 for di in range(1, target_variable.shape[0]):
                     decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
