85e737919f9600422f9ef2e2d22d4db96494fdb9
vasiliyeskin <vasiliyeskin@yandex.ru>
2020-10-23 05:37:04 +0300

    test

--- train_im2latex.py
+++ train_im2latex.py
@@ -100,7 +100,7 @@ def train(image, text, encoder, decoder, criterion, train_loader, teach_forcing_
             loss = 0.0
             teach_forcing = True if random.random() > teach_forcing_prob else False
             print(f'    teach_forcing: {0}', teach_forcing)
-            print(f'    batch_size condition: {0}', decoder_input.shape[0] < batch_size)
+            print(f'    decoder_input.shape[0] {0}, batch_size {1}, batch_size condition: {2}', decoder_input.shape[0], batch_size, decoder_input.shape[0] < batch_size)
             if teach_forcing or decoder_input.shape[0] < batch_size:
                 for di in range(1, target_variable.shape[0]):
                     decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs)
